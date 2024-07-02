76d259d97aac5b688137e646c71920b934bd4958
Mateo <Mateo350dk@gmail.com>
2024-07-02 12:27:46 -0300

    commit con C# primer push

---
+++ primerPrograma/primerPrograma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class PedirDatos
+{
+    public static void Main(string[] args){
+        Console.WriteLine("Introduzca los enteros: ");
+        int[] arreglo = {};
+        
+        for(int i = 0; i < arreglo.Length; i++)
+        {
+          
+            arreglo[i] = i;
+            Console.WriteLine(arreglo[i]);
+
+        }
+
+
+    }
+
+
+
+}
